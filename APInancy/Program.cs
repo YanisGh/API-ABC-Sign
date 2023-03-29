df36313d59ed5d034f3516bd86ec37ec97080b32
YanisGh <yanisghoul35@gmail.com>
2023-03-29 17:46:24 +0200

    debut bug

--- APInancy/Program.cs
+++ APInancy/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Nancy.Hosting.Self;
+using System;
+
+namespace APInancy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri("http://localhost:80")))
+            {
+                host.Start();
+
+                Console.WriteLine("NancyFX Stand alone test application.");
+                Console.WriteLine("Press enter to exit the application");
+                Console.ReadLine();
+            }
+        }
+    }
+}
