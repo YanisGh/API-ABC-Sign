3aaa24419f2d1d0c8cffc74ba1c2602532d47b1a
YanisGh <yanisghoul35@gmail.com>
2023-05-13 14:03:54 +0200

    connection to database

--- APInancy/Program.cs
+++ APInancy/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Nancy.Hosting.Self;
+using MySql.Data.MySqlClient;
 using System;
+using System.Data;
 
 namespace APInancy
 {
@@ -7,6 +9,21 @@ namespace APInancy
     {
         static void Main(string[] args)
         {
+            string connString = "server=localhost;user=root;password=;database=abcsignapi";
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            conn.Open();
+
+            if (conn.State == ConnectionState.Open)
+            {
+                Console.WriteLine("Connected to the database!");
+            }
+            else
+            {
+                Console.WriteLine("Not connected to the database.");
+            }
+
             using (var host = new NancyHost(new Uri("http://localhost:8080")))
             {
                 host.Start();
